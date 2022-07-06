bfccc2b7decec3a86b328a48f20b7bb78f733ddb
Christine Jackson <mom2MB464@gmail.com>
2022-07-06 17:59:33 -0400

    Cleaned code and deleted unneccessary files & folder

--- script.js
+++ script.js
@@ -146,23 +146,11 @@ function hideInputBox(column) {
 
 // ALLOWS ARRAYS TO REFLECT DRAG & DROP ITEMS
 function rebuildArrays() {
-  backlogListArray = [];
-  for (let i = 0; i < backlogList.children.length; i++) {
-    backlogListArray.push(backlogList.children[i].textContent);
-    }
-  progressListArray = [];
-  for (let i = 0; i < progressList.children.length; i++) {
-    progressListArray.push(progressList.children[i].textContent);
-    }
-  completeListArray = [];  
-  for (let i = 0; i < completeList.children.length; i++) {
-    completeListArray.push(completeList.children[i].textContent);
-    }
-  onHoldListArray = [];
-  for (let i = 0; i < onHoldList.children.length; i++) {
-    onHoldListArray.push(onHoldList.children[i].textContent);
-    }
-    updateDOM();
+  backlogListArray = Array.from(backlogListEl.children).map(i => i.textContent);
+  progressListArray = Array.from(progressListEl.children).map(i => i.textContent);
+  completeListArray = Array.from(completeListEl.children).map(i => i.textContent);
+  onHoldListArray = Array.from(onHoldListEl.children).map(i => i.textContent);
+  updateDOM();
 }
 
 // WHEN ITEM ENTERS COLUMN AREA
